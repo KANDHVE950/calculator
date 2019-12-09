533b95102f4140831aad67df296911230c8b1a7a
KANDHVE950 <kandhve@iitg.ac.in>
2019-12-10 03:38:01 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,27 @@
+// function getHistory() {
+// 	return document.getElementById("history-value").innerHTML;
+	
+// 	}
+
+// 	function printHistory(num) {
+// 		document.getElementById("history-value").innerHTML = num;
+// 	} 
+	
+// 	function getOutput() {
+// 		var x = document.getelementById("output-value");
+// 		return x;
+// 	}
+// 	// printHistory(6544);
+// 	function printOutput(num){
+// 		document.getelementById("output-value").innerHTML = num
+// 	}
+    function fun() {
+     var data = document.getelementById("hero")
+     data.addEventListener('click',function() {
+     	alert("You have clicked " + this.id);
+     })
+
+     }
+     
+     
+   
